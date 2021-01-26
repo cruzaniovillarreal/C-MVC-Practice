b1d9f790ecb89185aa61fbf06e2630fec5e9546f
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2021-01-26 10:51:58 -0600

    * .gitignore: C# MVC base created, Hello World JS added * C_MVC_Practice.sln: * Program.cs: * Startup.cs: * appsettings.json: * site.js: * favicon.ico: * site.css: * C_MVC_Practice.csproj: * Index.cshtml: * _ViewStart.cshtml: * HelloWorld.js: * ErrorViewModel.cs: * Privacy.cshtml: * Error.cshtml: * _ViewImports.cshtml: * _Layout.cshtml: * appsettings.Development.json: * HomeController.cs: * LICENSE: * LICENSE.txt: * launchSettings.json: * jquery.js: * jquery.min.js: * jquery.min.map: * LICENSE.md: * bootstrap.js: * bootstrap.css: * _ValidationScriptsPartial.cshtml: * bootstrap.min.js: * bootstrap.js.map: * bootstrap.css.map: * bootstrap.min.css: * bootstrap-grid.css: * bootstrap.bundle.js: * bootstrap.min.js.map: * bootstrap-reboot.css: * bootstrap.min.css.map: * LICENSE.txt: * bootstrap.bundle.js.map: * bootstrap.bundle.min.js: * jquery.validate.js: * bootstrap-grid.css.map: * bootstrap-grid.min.css: * bootstrap-reboot.css.map: * bootstrap-reboot.min.css: * additional-methods.js: * bootstrap.bundle.min.js.map: * jquery.validate.min.js: * bootstrap-grid.min.css.map: * bootstrap-reboot.min.css.map: * additional-methods.min.js: * jquery.validate.unobtrusive.js: * jquery.validate.unobtrusive.min.js:

---
+++ C_MVC_Practice/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace C_MVC_Practice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
